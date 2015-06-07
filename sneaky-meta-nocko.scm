90bfb940a0a12f423b9f0f0809258f95702e2978
James P. Torre, IV <torreaiworks01@gmail.com>
2015-06-07 20:18:26 +0000

    Removed snark.

--- sneaky-meta-nocko.scm
+++
@@ -1,8 +0,0 @@
-;;  Courtesy Gavin Whelan
-;;  UTC20150607
-
-;;  Destructures head and tail of subject, rebuilds and
-;;  applies. Reminiscent of using (eval) in one's meta-circular Scheme
-;;  interpreter.
-(define clever-meta-nocko
-  '[(num (0 1)) [[(num (0)) (num (0 1))] [(num (0)) (num (1 1))]]])
