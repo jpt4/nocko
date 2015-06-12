a8382f0168266a4b5ff8a19c0bc468489ec500d6
James P. Torre, IV <torreaiworks01@gmail.com>
2015-06-12 04:15:21 +0000

    Branch test

--- meta-nock.scm
+++ meta-nock.scm
@@ -2,6 +2,9 @@
 ;;  (meta-nock), meta-circular evaluator for nock expressions, written in 
 ;;  nock, interpreted by (nocko)
 
+;;  TODO: IMPLEMENT MINIKANREN DECIMAL NUMERALS
+(define meta-nocko (preprocess meta-nock))
+
 (define meta-nock
 
-(define meta-nocko (preprocess meta-nock))
+
