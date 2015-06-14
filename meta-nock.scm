a81cd29977cb962a259d586e7408cf36b4037c58
James P. Torre, IV <torreaiworks01@gmail.com>
2015-06-14 10:52:35 +0000

    Streak test

--- meta-nock.scm
+++ meta-nock.scm
@@ -2,8 +2,9 @@
 ;;  (meta-nock), meta-circular evaluator for nock expressions, written in 
 ;;  nock, interpreted by (nocko)
 
-;;  TODO: IMPLEMENT MINIKANREN DECIMAL NUMERALS
-
+;;  [b a] ncond - eval set b of [test consequence] pairs against a, eval as 
+;;  nested redex 6 in order.
+(define ncond `[9 b [8
 
 (define rasf '[1 1])
 (define rast `[1 0])
@@ -11,4 +12,5 @@
 (define meta-nock
 `[6 [3 0 2] ,rast ,rasf])
 
+;;  TODO: IMPLEMENT MINIKANREN DECIMAL NUMERALS
 (define meta-nocko (preprocess meta-nock))
